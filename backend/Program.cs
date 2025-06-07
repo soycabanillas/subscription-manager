124c42c1f4c11936ffc12180ab2b181df248b72f
Alejandro Cabanillas <soycabanillas@gmail.com>
2025-06-07 18:40:13 +0100

    Refactor project structure and configuration for OutsetAPI; update solution paths, remove obsolete files, and add new launch and task configurations.

--- backend/Program.cs
+++ backend/Program.cs
@@ -25,7 +25,7 @@ Todo[] sampleTodos =
     new(1, "Walk the dog"),
     new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
     new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
-    new(4, "Clean the bathroom"),
+    new(4, "Clean www the bathroom"),
     new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
 ];
 
